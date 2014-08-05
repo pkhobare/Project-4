3c42db120545c75efa2459273950bbc0915d7bed
Paritosh Khobare <pkhobare@g.harvard.edu>
2014-08-05 16:15:20 -0400

    Production server setup

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'hostname-goes-here',
+            'database'  => 'docFinder',
+            'username'  => 'margarette',
+            'password'  => 'iSrRpZVn',
+        ),
+
+    ),
+
+);
